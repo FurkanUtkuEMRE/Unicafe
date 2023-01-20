18fb01473a7afefe75697666c287e9c0357e182f
FurkanUtkuEMRE <furkanutkuemre@hotmail.com>
2023-01-20 19:05:48 +0300

    feat: finalize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [goodVotes, setGoodVotes] = useState(0);
+  const [neutralVotes, setNeutralVotes] = useState(0);
+  const [badVotes, setBadVotes] = useState(0);
+  const [allVotes, setAllVotes] = useState(0);
+
+  const handleGoodVotes = () => {
+    setGoodVotes(goodVotes + 1);
+    setAllVotes(allVotes + 1);
+  };
+
+  const handleNeutralVotes = () => {
+    setNeutralVotes(neutralVotes + 1);
+    setAllVotes(allVotes + 1);
+  };
+
+  const handleBadVotes = () => {
+    setBadVotes(badVotes + 1);
+    setAllVotes(allVotes + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGoodVotes} buttonText={'Vote Good'}></Button>
+      <Button onClick={handleNeutralVotes} buttonText={'Vote Neutral'}></Button>
+      <Button onClick={handleBadVotes} buttonText={'Vote Bad'}></Button>
+
+      <h1>Statistics</h1>
+      {goodVotes || neutralVotes || badVotes ? (
+        <table>
+          <td>
+            <p>Good Votes:</p>
+            <p>Neutral Votes:</p>
+            <p>Bad Votes:</p>
+            <p>All Votes:</p>
+            <p>Vote Percentage:</p>
+            <p>Average Vote:</p>
+          </td>
+          <td>
+            <Statistics statisticsValue={goodVotes}></Statistics>
+            <Statistics statisticsValue={neutralVotes}></Statistics>
+            <Statistics statisticsValue={badVotes}></Statistics>
+            <Statistics statisticsValue={allVotes}></Statistics>
+            <Statistics statisticsValue={`${((100 * goodVotes) / allVotes).toFixed(2)}%`}></Statistics>
+            <Statistics statisticsValue={((goodVotes - badVotes) / allVotes).toFixed(2)}></Statistics>
+          </td>
+        </table>
+      ) : (
+        <Statistics statisticsText={`No feedback yet.`}></Statistics>
+      )}
+    </>
   );
-}
+};
+
+const Button = ({ onClick, buttonText }) => <button onClick={onClick}>{buttonText}</button>;
+
+const Statistics = ({ statisticsValue }) => <p>{statisticsValue}</p>;
 
 export default App;
